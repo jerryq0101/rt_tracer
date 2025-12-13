a57a438112ddd00a1665686201f28927d1b7944e
jerry utm machine <jerryq0501@gmail.com>
2025-12-13 04:09:52 +0000

    add case studies first one

---
+++ case_studies/1_misconfig_priority/control_loop.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <time.h>     // clock_gettime(), struct timespec
+#include <stdint.h>   // int64_t / uint64_t if you use them
+
+#define PERIOD_NS 2000000  // 2ms
+
+static inline void ts_add_ns(struct timespec *ts, long ns) {
+    ts->tv_nsec += ns;
+    while (ts->tv_nsec >= 1000000000L) {
+        ts->tv_nsec -= 1000000000L;
+        ts->tv_sec++;
+    }
+}
+
+volatile unsigned long sum = 0;
+
+/**
+ * This loop simulates a fast control loop that does some computation every 2ms.
+ */
+int main(void) {
+        struct timespec next;
+        clock_gettime(CLOCK_MONOTONIC, &next);
+        // Initially Next is the current time
+
+        for (;;) {
+                // Schedule next period
+                ts_add_ns(&next, PERIOD_NS);
+
+                // Do some some simple thing to keep compiler happy
+                sum = sum % 100000000 + 1;
+                
+                // Sleep until next period
+                clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next, NULL);
+        }
+        
+        return 0;
+}
